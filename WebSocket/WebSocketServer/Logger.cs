ec9201cfc0b67d6a2e2d12008efbfe774bf67bdb
dongshuiluo <dongshuiluo@gmail.com>
2014-11-22 22:28:36 +0800

    first
    first

---
+++ WebSocket/WebSocketServer/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSocketServer
+{
+    public class Logger
+    {
+        public bool LogEvents { get; set; }
+
+        public Logger()
+        {
+            LogEvents = true;
+        }
+
+        public void Log(string Text)
+        {
+            if (LogEvents) 
+                Console.WriteLine(Text);
+        }
+    }
+}
---
+++ WebSocket/WebSocketServer/WebSocketsServerTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+namespace WebSocketServer
+{
+    class WebSocketServerTest : IDisposable
+    {
+        private WebSocketServer WSServer;
+        public WebSocketServerTest()
+        {
+            //使用默认的设置
+            WSServer = new WebSocketServer();
+        }
+
+        public void Dispose()
+        {
+            Close();
+        }
+
+        private void Close()
+        {
+            WSServer.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        ~WebSocketServerTest()
+        {
+            Close();
+        }
+
+        public void Start()
+        {
+            WSServer.StartServer();
+        }
+    }
+}
